5837ce455a1d7a8b2293fe7d1637ea26b2e2b845
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2025-03-30 10:44:44 +0200

    add Header component and integrate routing with StylesProvider

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({ productionPrefix: 'co' });
 
 const App = ({}) => {
   return (
-    <div>
-      <h1>Hi there!!!!!</h1>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
 
