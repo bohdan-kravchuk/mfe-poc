33f971c702259a12be1e878d92c9025708a5cdd5
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2025-03-30 12:09:42 +0200

    remove duplicate import of StylesProvider from App

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
